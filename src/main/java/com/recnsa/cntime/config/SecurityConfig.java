4b2139764cbb4bf5c81e6ed8bde373a358b77276
Chaerim <cofla2020@yonsei.ac.kr>
2024-09-27 00:24:56 +0900

    feat: set corsconfig

--- src/main/java/com/recnsa/cntime/config/SecurityConfig.java
+++ src/main/java/com/recnsa/cntime/config/SecurityConfig.java
@@ -13,6 +13,13 @@ import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationF
 import org.springframework.security.oauth2.jwt.JwtValidationException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.SecurityContextHolderFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource; // servlet 기반
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -36,5 +43,32 @@ public class SecurityConfig {
                 .addFilterAfter(new JwtValidationFilter(objectMapper), SecurityContextHolderFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
+                        .allowedOrigins("http://localhost:3000", "https://ppomoppomo.netlify.app") // 허용할 출처 설정
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드 설정
+                        .allowedHeaders("*") // 허용할 헤더 설정
+                        .allowCredentials(true); // 쿠키와 함께 요청 허용
+            }
+        };
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // 허용할 출처 설정
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드 설정
+        configuration.setAllowedHeaders(List.of("*")); // 허용할 헤더 설정
+        configuration.setAllowCredentials(true); // 쿠키 등 인증 정보 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 설정 적용
+        return source;
     }
 }
