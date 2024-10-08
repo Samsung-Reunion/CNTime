4b32d45a9844320bd9e8a2d8fa543b2920cdd6c1
RaccHoon <choco730522@gmail.com>
2024-10-08 12:02:30 +0900

    hotfix: fix cors error and redirect uri

--- src/main/java/com/recnsa/cntime/config/SecurityConfig.java
+++ src/main/java/com/recnsa/cntime/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // 허용할 출처 설정
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://ppomoppomo.nestify.app")); // 허용할 출처 설정
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드 설정
         configuration.setAllowedHeaders(List.of("*")); // 허용할 헤더 설정
         configuration.setAllowCredentials(true); // 쿠키 등 인증 정보 허용
